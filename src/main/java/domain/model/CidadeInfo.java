fafadbd03044b5e1125abcad0504a536285e7d42
Rafael Oliveira <rafael.oliveira@m4sistemas.com.br>
2022-12-22 12:12:07 -0300

    Classe CidadeInfo

--- src/main/java/domain/model/CidadeInfo.java
+++ src/main/java/domain/model/CidadeInfo.java
@@ -1,5 +1,28 @@
 package domain.model;
 
-public class CidadeInfo {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class CidadeInfo {
+	
+	@JsonProperty("area")
+	private String area;
+	
+	@JsonProperty("codigo_ibge")
+	private String codigoIbge;
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getCodigoIbge() {
+		return codigoIbge;
+	}
+
+	public void setCodigoIbge(String codigoIbge) {
+		this.codigoIbge = codigoIbge;
+	}
 }
