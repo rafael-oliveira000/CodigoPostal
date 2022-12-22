5f156cbc448cac140d306c1892f2e86afb38bcad
Rafael Oliveira <rafael.oliveira@m4sistemas.com.br>
2022-12-22 12:07:43 -0300

    Classe Principal

--- src/main/java/domain/model/Principal.java
+++ src/main/java/domain/model/Principal.java
@@ -1,5 +1,61 @@
 package domain.model;
 
-public class Principal {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Principal {
+	
+	@JsonProperty("bairro")
+	private String bairro;
+	@JsonProperty("cep")
+	private String cep;
+	@JsonProperty("cidade")
+	private String cidade;
+	@JsonProperty("estado")
+	private String estado;
+	
+	@JsonProperty("cidade_info")
+	private CidadeInfo cidadeInfo;
+	
+	@JsonProperty("estado_info")
+	private EstadoInfo estadoInfo;
+	
+	
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public CidadeInfo getCidadeInfo() {
+		return cidadeInfo;
+	}
+	public void setCidadeInfo(CidadeInfo cidadeInfo) {
+		this.cidadeInfo = cidadeInfo;
+	}
+	public EstadoInfo getEstadoInfo() {
+		return estadoInfo;
+	}
+	public void setEstadoInfo(EstadoInfo estadoInfo) {
+		this.estadoInfo = estadoInfo;
+	}
+
+	
 }
