cd79938cd8135cb24af2c171c0072816977a343f
Rafael Oliveira <rafael.oliveira@m4sistemas.com.br>
2022-12-22 11:54:32 -0300

    First commit

---
+++ src/test/java/com/codigopostal/CodigopostalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigopostal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigopostalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
